b44d9d2e644b1e6cb59c48c98477c0ad9e1d0aef
Kenan Amundsen Elkoca <aa312@intility.no>
2017-02-17 14:04:02 +0100

    added master rickroll v1

---
+++ MasterRickRollV1.ps1
@@ -0,0 +1,42 @@
+$code = @"
+    [DllImport("user32.dll")]
+    public static extern bool BlockInput(bool fBlockIt);
+"@
+
+$userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
+
+function Disable-UserInput($seconds) {
+    $userInput::BlockInput($true)
+    Start-Sleep $seconds
+    $userInput::BlockInput($false)
+}
+
+
+#Disable-UserInput -seconds 10 | Out-Null
+
+function Rick-Roll {
+	[CmdletBinding()]
+	param (
+	)
+	
+	begin {
+
+	}
+	
+	process {
+		$ie = New-Object -ComObject InternetExplorer.Application
+		$ie.Navigate("https://tinyurl.com/6dlylhn")
+
+		$ie2 = New-Object -ComObject InternetExplorer.Application
+		$ie2.visible = $true
+		$ie2.fullscreen = $true
+		$ie2.navigate("http://giphy.com/gifs/pandawhale-rickroll-pandawhalegif-szpY5c63cmzOE/fullscreen")
+		Disable-UserInput -seconds 10 | Out-Null
+		Get-Process -Name ie* | stop-process -Force
+	}
+	
+	end {
+	}
+}
+
+Rick-Roll
